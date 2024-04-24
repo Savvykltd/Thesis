596e87c0c3b54cb081ce0ee21543e243d98e4286
Severin <hidajats@mcmaster.ca>
2024-04-24 19:31:19 -0400

    Update Test.ipynb: add imports for branca, folium, and mapclassify; use graph\_to\_gdfs from convert module; add code to find nearest node to a given latitude and longitude. Got number of lanes from lat and long

--- Code/Code.py
+++ Code/Code.py
@@ -1,10 +1,15 @@
-import cv
-
+import cv2 as cv
 import numpy as np
 import matplotlib.pyplot as plt
 
 def canny(frame):
-    image_gray = cv.cvtColor(frame, cv.COLOR_RGB2GRAY)
+    image_gray =  cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
+    blurred = cv.GaussianBlur(image_gray,(5,5),0)
+    
+    # Parameters for Canny Edge Detection
+    low_threshold = 100   # The minimum value for the hysteresis threshold:
+                        # any edge with a weight lower than this value will be discarded.
+    high_threshold = 300  # The maximum value for the hysteresis threshold. An edge is considered strong if the weight of all the pixels in the nx
     image_blur = cv.GaussianBlur(image_gray, (5, 5),0)
     canny = cv.Canny(image_blur, 50, 150)
 
@@ -21,4 +26,26 @@ def draw_lines(frame, lines):
         cv.line(frame, (x1,y1), (x2,y2), [0,255,0], 2)
 
 #Create a image reading in files in the folder
+cap = cv.VideoCapture("project_video.mp4")
+
+while(True):
+    ret, frame = cap.read()
+    
+    # if not ret:
+    #     print("end of video file...")
+    #     break
+    canny(frame)
+    cropped_img = segment(frame)
+    gray = cv.cvtColor(cropped_img, cv.COLOR_BGR2GRAY)
+    edges = cv.Canny(gray, 50, 150)
+    lines = cv.HoughLinesP(edges, cv.HOUGH_PROBABILISTIC, dp=18, minLineLength=40, maxLineGap=50)
+    draw_lines(frame, lines)
+    cv.imshow("Frame", frame)
+    key = cv.waitKey(1) & 0xFF
+    if key == ord("q"):
+        break
+    cv.destroyAllWindows()
+
+
+
 
